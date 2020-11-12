323a5834d496a87a1eef51de4e6f0bfaa02073a2
Head8che <hagi@ualberta.ca>
2020-11-12 03:52:29 -0700

    feat/refact: Commands for the following: View/Update: Workspace Rules; View Admin; Update/View Specific Roles; View FAQ

--- index.js
+++ index.js
@@ -1,123 +1,373 @@
-require('dotenv').config();
-const SlackBot = require('slackbots');
+const { App } = require("@slack/bolt");
+const { forEach } = require("lodash");
+require("dotenv").config(); 
 const _ = require('lodash');
-const axios = require('axios');
-const express = require('express');
-const bodyParser = require('body-parser');
-const request = require("request");
 
-// Creates express (middleware)
-const app = express();
+const app = new App({
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  token: process.env.SLACK_BOT_TOKEN,
+  name: "Ada's Bot"
+}); 
 
-// The port used for express server
-const PORT = 3000;
-
-// listen/runs server
-app.listen(process.env.PORT || PORT, function() {
-    console.log('Bot is listening on port ' + PORT);
-});
-
-const bot = new SlackBot({
-    token: process.env.SLACK_AUTH_TOKEN,
-    name: "Ada's Bot"
-});
-
-var listofChannels = []
-// Placeholder Data
-var rolesDescription = "*The various roles are:* \n" + "*Mentors* : [placeholder]\n" + "*Admins*: [placeholder]\n"
-var workspaceRules = "Update your rules:"
-var sendTrainingMaterial = "[placeholder_training]"
-var supplementalResources = "[placeholder_supplementalResources]"
-var faq = "[placeholder_faq]"
+(async () => {
+    await app.start(process.env.PORT); // Starts the bot
+    console.log("Bot is listening on port " + process.env.PORT);
+})();
 
 
-// Starting handler
-bot.on('start', () => {
-    updateChannels()
-    // bot.postMessageToChannel('testing',  "Welcome to Ada's Team Workspace");
-});
-
-bot.on('error', err => console.log(err));
-
-//Message Handler
-bot.on('message',(data) => {
-    console.log(data)
-    // Checkign if a member has joined the welcoming channel
-    if(data.subtype === 'channel_join')
-    {
-        // console.log(data)
-        // bot.getUser(data.user).then((name) => console.log(name))
-        publicGreeting(data.user)
+app.command('/update_workspace_rules',  async ({ ack, body, client }) => {
+    client.s 
+    await ack();
+    var adminsID = _.map(adminList,userAdmin => userAdmin.id)
+    if (adminsID.includes(body.user_id)){
+        try {
+                const result = await client.views.open({
+                    trigger_id: body.trigger_id,
+                    // Payload
+                    view: {
+                    type: 'modal',
+                    callback_id: 'view_1',
+                    title: {
+                        type: 'plain_text',
+                        text: 'Update Workspace Rules'
+                    },
+                    blocks: [
+                        {
+                        type: 'input',
+                        block_id: 'input_c',
+                        label: {
+                            type: 'plain_text',
+                            text: 'Rules'
+                        },
+                        element: {
+                            type: 'plain_text_input',
+                            action_id: 'inputField',
+                            multiline: true
+                        }
+                        }
+                    ],
+                    submit: {
+                        type: 'plain_text',
+                        text: 'Submit'
+                    }
+                    }
+                });
+                // console.log(result);
+                }
+            catch (error) {
+                console.error(error);
+            }
     }
-    if(data.subtype === 'channel_created'){
-        updateChannels()
+    else{
+                // try {
+        //     await app.client.chat.postMessage({
+        //       token: context.botToken,
+        //       channel: user,
+        //       text: msg
+        //     });
+        //   }
+        //   catch (error) {
+        //     console.error(error);
+        //   }
+        ///// Not Authorized User /////
     }
 });
 
-
-function publicGreeting(userID){
-    // bot.getUserById(user).then(() => console.log(name))
-    bot.getUserById(userID).then( user => {
-        var greeting = `Welcome to Ada's Team Workspace ${user.name}`
-        bot.postMessageToChannel('testing',  greeting);
-    })
-}
-
-
-// *** promise bug *** // Leave for me 
-async function updateChannels(){
-    listofChannels = []
-    bot.getChannels().then( allChannels => {
-        allChannels.channels.forEach(channel => { 
-            listofChannels.push(channel.name)     
-        });
-    })
-    return listofChannels; // returns a promise of list of channels, once it gets fulfilled.
-}
-
-
-// Create command that shows Instructions on how to update personal information on Slack
-app.post("/update info", function(req, res) { 
-    res.send("This is a post request to '/update'!!\n" + listofChannels); 
-}); 
-
-// Create command to view supplemental resources 
-app.post("/resources", function(req, res) { 
-    res.send(supplementalResources); 
-});
-
-// Create command to view FAQ sheet for workspaces  
-app.post("/faq", function(req, res) { 
-    res.send(faq); 
-}); 
-
-// Create command to view workspace rules 
-app.post("/rules", function(req, res) { 
-    res.send(workspaceRules); 
-}); 
+var rules = 'No Rules Currently Set'
+app.view('view_1', async ({ ack, body, view, context }) => {
+    await ack();
+    const val = view['state']['values']['input_c'];
+    const user = body['user']['id'];
+    rules = val.inputField.value
   
-// Create command to view training materials 
-app.post("/training", function(req, res) { 
-    res.send(sendTrainingMaterial); 
+    // Message to send to the sending user
+    let msg = '';
+      msg = 'Your submission was successful';
+
+    // Message
+    try {
+      await app.client.chat.postMessage({
+        token: context.botToken,
+        channel: user,
+        text: msg
+      });
+    }
+    catch (error) {
+      console.error(error);
+    }
+  
 });
-
-// Create command to display information about specific roles 
-app.post("/roles", function(req, res) { 
-    res.send(rolesDescription); 
-    
-}); 
-
-// Create command to view admins // Data is the bot obtains whenever it is started  
-app.post("/admins", function(req, res) { 
-    bot.getUsers().then( users => {
-        var admins = _.filter(users.members,['is_admin',true]).map(userAdmin => userAdmin.real_name)
-        res.send("The following admins of this workspace are: \n"+ (admins)); 
+  
+app.command('/workspace_rules', async ({ ack, body, say }) => {
+    await ack();
+    // const regex = "\n* /gi"
+    // console.log(rules.replace(regex,"\n"))
+    await app.client.chat.postEphemeral({
+        token: process.env.SLACK_BOT_TOKEN,
+        channel: body.channel_id,
+        user: body.user_id,
+        text: `*Current Workspace Rules:* \n ${rules}`
     })
+
+})
+  
+app.command('/resources', async ({ ack, body, say }) => {
+    await ack();
+    console.log("Resources")
+    await app.client.chat.postEphemeral({
+        token: process.env.SLACK_BOT_TOKEN,
+        channel: body.channel_id,
+        user: body.user_id,
+        text: `Resources`
+    })
+    
+})
+
+app.command('/faq', async ({ ack, body, say }) => {
+    await ack();
+    await app.client.chat.postEphemeral({
+        token: process.env.SLACK_BOT_TOKEN,
+        channel: body.channel_id,
+        user: body.user_id,
+        text: "Read our FAQ here https://www.adasteam.ca/faq"
+    })
+})
+  
+app.command('/update_roles',  async ({ ack, body, client }) => {
+    await ack();
+    var adminsID = _.map(adminList,userAdmin => userAdmin.id)
+    if (adminsID.includes(body.user_id)){
+        try {
+                const result = await client.views.open({
+                    trigger_id: body.trigger_id,
+                    view: {
+                    type: 'modal',
+                    callback_id: 'view_2',
+                    title: {
+                        type: 'plain_text',
+                        text: 'Update Workspace Roles'
+                    },
+                    blocks: [
+                        {
+                            "type": "input",
+                            block_id: 'input_td',
+                            "element": {
+                                "type": "plain_text_input",
+                                "action_id": "title",
+                                "placeholder": {
+                                    "type": "plain_text",
+                                    "text": "What do you want to ask of the world?"
+                                }
+                            },
+                            "label": {
+                                "type": "plain_text",
+                                "text": "Title"
+                            }
+                        },
+                        
+                        {
+                        type: 'input',
+                        block_id: 'input_d',
+                        label: {
+                            type: 'plain_text',
+                            text: 'Description'
+                        },
+                        element: {
+                            type: 'plain_text_input',
+                            action_id: 'inputField',
+                            multiline: true
+                        }
+                        },
+                        
+                        {
+                            "type": "input",
+                            block_id: 'input_te',
+                            "element": {
+                                "type": "plain_text_input",
+                                "action_id": "title",
+                                "placeholder": {
+                                    "type": "plain_text",
+                                    "text": "What do you want to ask of the world?"
+                                }
+                            },
+                            "label": {
+                                "type": "plain_text",
+                                "text": "Title"
+                            }
+                        },
+                        
+                        {
+                        type: 'input',
+                        block_id: 'input_e',
+                        label: {
+                            type: 'plain_text',
+                            text: 'Description'
+                        },
+                        element: {
+                            type: 'plain_text_input',
+                            action_id: 'inputField',
+                            multiline: true
+                        }
+                        },
+
+
+                        {
+                            "type": "input",
+                            block_id: 'input_tf',
+                            "element": {
+                                "type": "plain_text_input",
+                                "action_id": "title",
+                                "placeholder": {
+                                    "type": "plain_text",
+                                    "text": "What do you want to ask of the world?"
+                                }
+                            },
+                            "label": {
+                                "type": "plain_text",
+                                "text": "Title"
+                            }
+                        },
+                        
+                        {
+                        type: 'input',
+                        block_id: 'input_f',
+                        label: {
+                            type: 'plain_text',
+                            text: 'Description'
+                        },
+                        element: {
+                            type: 'plain_text_input',
+                            action_id: 'inputField',
+                            multiline: true
+                        }
+                        },
+                    ],
+                    submit: {
+                        type: 'plain_text',
+                        text: 'Submit'
+                    }
+                    }
+                });
+                // console.log(result);
+                }
+            catch (error) {
+                console.error(error);
+            }
+    }
+    else{
+        // try {
+        //     await app.client.chat.postMessage({
+        //       token: context.botToken,
+        //       channel: user,
+        //       text: msg
+        //     });
+        //   }
+        //   catch (error) {
+        //     console.error(error);
+        //   }
+        ///// Not Authorized User /////
+    }
 });
 
-// Create command to update workspace rules 
-app.post("/update_rules", function(req, res) { 
-    console.log(req,res)
-    res.send(workspaceRules); 
-}); 
+var roles = 'No Roles Are Currently Set'
+app.view('view_2', async ({ ack, body, view, context }) => {
+    await ack();
+    const key1 = view['state']['values']['input_td']['title']['value'];
+    const val1 = view['state']['values']['input_d']['inputField']['value'];
+    const key2 = view['state']['values']['input_te']['title']['value'];
+    const val2 = view['state']['values']['input_e']['inputField']['value'];
+    const key3 = view['state']['values']['input_tf']['title']['value'];
+    const val3 = view['state']['values']['input_f']['inputField']['value'];
+
+    roles = {}
+    roles[key1] = val1;
+    roles[key2] = val2;
+    roles[key3] = val3;
+
+    const user = body['user']['id'];
+    // rules = val.inputField.value
+  
+    let msg = '';
+
+    msg = 'Your submission was successful';
+
+
+    try {
+      await app.client.chat.postMessage({
+        token: context.botToken,
+        channel: user,
+        text: msg
+      });
+    }
+    catch (error) {
+      console.error(error);
+    }
+});
+
+
+app.command('/roles', async ({ ack, body, say }) => {
+    await ack();
+    var roles_type = ''
+    for (const [key, value] of Object.entries(roles)) {
+        // console.log(`${key}: ${value}`);
+        roles_type += `*${key}*: \n ${value} \n\n`
+      }
+      await app.client.chat.postEphemeral({
+        token: process.env.SLACK_BOT_TOKEN,
+        channel: body.channel_id,
+        user: body.user_id,
+        text: `The following admins of this workspace are: \n ${(roles_type)}`
+    })
+})
+
+app.command('/training', async ({ ack, body, say }) => {
+    await ack();
+    console.log(body)
+    await app.client.chat.postEphemeral({
+        token: process.env.SLACK_BOT_TOKEN,
+        channel: body.channel_id,
+        user: body.user_id,
+        text: `Training`
+    })
+})
+
+
+app.command('/admins', async ({ ack, body, say }) => {
+    await ack();
+    fetchUsers();
+    var admins = _.map(adminList,userAdmin => userAdmin.real_name)
+    await app.client.chat.postEphemeral({
+        token: process.env.SLACK_BOT_TOKEN,
+        channel: body.channel_id,
+        user: body.user_id,
+        text: `The following admins of this workspace are: \n ${(admins)}`
+    })
+})
+  
+var adminList = {};
+async function fetchUsers() {
+  try {
+    const result = await app.client.users.list({
+      token: process.env.SLACK_BOT_TOKEN
+    });
+
+    saveUsers(result.members);
+  }
+  catch (error) {
+    console.error(error);
+  }
+}
+
+
+function saveUsers(usersArray) {
+  var userId = '';
+  usersArray.forEach(function(user){
+    if (user["is_admin",true]){
+        userId = user["id"];
+    }
+    adminList["Admin"] = user;
+  });
+}
+
+fetchUsers();
 
