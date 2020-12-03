86d073940b6cb1dbbf68565e00f4861f5aa92134
M.Abdullah <m26abdullah@gmail.com>
2020-12-02 17:51:27 -0800

    feat: working /resources command with backend dictionary

--- config/constants.js
+++ config/constants.js
@@ -4,31 +4,58 @@ const updateInfo = {
 }
 
 database = {
-    adasmentors : {
-        welcomeMessage : "Welcome to the Ada’s Mentors workspace! The goal of Ada’s Mentors is to mentor underprivileged students lacking the confidence and exposure to university and the application process.",
+    "adasmentors" : {
+        welcomeMessage : 
+            "Welcome to the Ada’s Mentors workspace! The goal of Ada’s Mentors is to mentor underprivileged students lacking the confidence and exposure to university and the application process.",
         workspaceRules : ['Currently there are no rules in this Workspace'],
         roles : [],
         training : [],
-        resources : {adasMentorsWebsite:{url: "https://sites.google.com/ualberta.ca/adas-mentors-wiki/home"}},
-        acknowledgements : {body : "We respectfully acknowledge that Ada’s Mentors is located on Treaty 6 territory. Treaty 6 is a traditional gathering place for diverse Indigenous peoples including the Cree, Blackfoot, Metis, Nakota Sioux, Iroquois, Dene, Ojibway/Saulteaux/Anishinaabe, Inuit, and many others whose histories, languages, and cultures continue to influence our vibrant community.", sig:  "Created by Ada’s Developers ", url: "https://github.com/adasdevelopers/slack-onboarding-bot"}
+        resources : {
+            adasMentorsWebsite:{
+                url: "https://sites.google.com/ualberta.ca/adas-mentors-wiki/home", 
+                text: "Check out Adas Mentors Wiki Home "
+            }},
+        acknowledgements : {
+            body : "We respectfully acknowledge that Ada’s Mentors is located on Treaty 6 territory. Treaty 6 is a traditional gathering place for diverse Indigenous peoples including the Cree, Blackfoot, Metis, Nakota Sioux, Iroquois, Dene, Ojibway/Saulteaux/Anishinaabe, Inuit, and many others whose histories, languages, and cultures continue to influence our vibrant community.", 
+            sig:  "Created by Ada’s Developers ", 
+            url: "https://github.com/adasdevelopers/slack-onboarding-bot"
+        }
     },
-    adasdevelopers : {
+    "adasdevelopers" : {
         welcomeMessage : "Welcome to Ada’s Developers! This workspace was created for the Ada’s Developers initiative, where students come together to create software! This workspace is accessible to software developers, designers, project managers, mentors, and administrators. If you have any questions, refer to this resource or ask one of the administrators for help!",
-        workspaceRules : ['✔️DO Use welcoming language and inclusive language', '✔️DO Be respectful of differing viewpoints and experiences' , '✔️DO Gracefully accept constructive criticism', '✔️DO Focus on what is best for the community', '✔️DO Show empathy towards other community members', '✔️DO NOT Use sexualized language or imagery', '✔️DO NOT Initiate sexual attention or advances' , '❌DO NOT Engage in trolling or make insulting/derogatory comments', '❌DO NOT Attack personal or political values', '❌DO NOT Engage in public or private harassment', '❌DO NOTPublish others private information, such as a physical or electronic address ', '❌DO NOT Engage in any other conduct which could reasonably be considered inappropriate in a professional setting'],
+        workspaceRules : [
+            '✔️DO Use welcoming language and inclusive language', 
+            '✔️DO Be respectful of differing viewpoints and experiences' , 
+            '✔️DO Gracefully accept constructive criticism', 
+            '✔️DO Focus on what is best for the community', 
+            '✔️DO Show empathy towards other community members', 
+            '✔️DO NOT Use sexualized language or imagery', 
+            '✔️DO NOT Initiate sexual attention or advances' , 
+            '❌DO NOT Engage in trolling or make insulting/derogatory comments', 
+            '❌DO NOT Attack personal or political values', 
+            '❌DO NOT Engage in public or private harassment', 
+            '❌DO NOTPublish others private information, such as a physical or electronic address ', 
+            '❌DO NOT Engage in any other conduct which could reasonably be considered inappropriate in a professional setting'],
         roles : [],
         training : [],
         resources : {},
-        acknowledgements : {body : "We respectfully acknowledge that Ada’s Developers is located on Treaty 6 territory. Treaty 6 is a traditional gathering place for diverse Indigenous peoples including the Cree, Blackfoot, Metis, Nakota Sioux, Iroquois, Dene, Ojibway/Saulteaux/Anishinaabe, Inuit, and many others whose histories, languages, and cultures continue to influence our vibrant community.", sig:  "Created by Ada’s Developers ", url: "https://github.com/adasdevelopers/slack-onboarding-bot"}
+        acknowledgements : {
+            body : "We respectfully acknowledge that Ada’s Developers is located on Treaty 6 territory. Treaty 6 is a traditional gathering place for diverse Indigenous peoples including the Cree, Blackfoot, Metis, Nakota Sioux, Iroquois, Dene, Ojibway/Saulteaux/Anishinaabe, Inuit, and many others whose histories, languages, and cultures continue to influence our vibrant community.", 
+            sig:  "Created by Ada’s Developers ", 
+            url: "https://github.com/adasdevelopers/slack-onboarding-bot"}
     },
-    adasteam :{
+    "adasteam" :{
         welcomeMessage : "Welcome to Ada’s Team! The Ada's Team workspace is for the executives to collaborate, ask questions, and fulfill Ada's Team initiatives. Although everyone has their VP roles to complete, the Ada's Team executive committee is meant to be a safe space; if you are struggling with your work, please ask others for help! Congrats, and thanks for joining our team. We're so happy to have you here with us!",
         workspaceRules : ['View our constitution, under Article 4 for the official rules and duties of an Ada’s Team executive:  https://drive.google.com/file/d/1ntfk7iUron6_jWnJSkhcQnpyn7iQbMy2/view?usp=sharing'],
         roles : [],
         training : [],
-        resources : {adasTeamExecResource : {url : "https://sites.google.com/ualberta.ca/adas-team-wiki/home"}},
+        resources : {
+            adasTeamExecResourceWebsite : {
+                url : "https://sites.google.com/ualberta.ca/adas-team-wiki/home", 
+                text : "Check out Adas Team Wiki Home "}},
         acknowledgements : {body : "We respectfully acknowledge that Ada’s Team is located on Treaty 6 territory.Treaty 6 is a traditional gathering place for diverse Indigenous peoples including the Cree, Blackfoot, Metis, Nakota Sioux, Iroquois, Dene, Ojibway/Saulteaux/Anishinaabe, Inuit, and many others whose histories, languages, and cultures continue to influence our vibrant community.", sig:  "Created by Ada’s Developers ", url: "https://github.com/adasdevelopers/slack-onboarding-bot"}
     },
-    adasnetwork: {
+    "adasnetwork": {
         welcomeMessage : "Welcome to Ada’s Network! This workspace was created to keep those involved with Ada’s Team, past or present, connected. It’s a supportive network that strives to provide a space for knowledge and opportunities to be shared with one another that empower our academic or career choices. We’re so happy to have you here with us!",
         workspaceRules : ['✔️DO Use welcoming language and inclusive language', '✔️DO Be respectful of differing viewpoints and experiences' , '✔️DO Gracefully accept constructive criticism', '✔️DO Focus on what is best for the community', '✔️DO Show empathy towards other community members', '✔️DO NOT Use sexualized language or imagery', '✔️DO NOT Initiate sexual attention or advances' , '❌DO NOT Engage in trolling or make insulting/derogatory comments', '❌DO NOT Attack personal or political values', '❌DO NOT Engage in public or private harassment', '❌DO NOTPublish others private information, such as a physical or electronic address ', '❌DO NOT Engage in any other conduct which could reasonably be considered inappropriate in a professional setting'],
         roles : [],
@@ -38,7 +65,14 @@ database = {
     
     }
 }
+workspaceChecker = {
+    'adas-teamworkspace' : "adasmentors",
+    'adas-developerworkspace': "adasdevelopers",
+    'adas-mentors-workspace': "adasmentors",
+    'adas-network-workspace' : "adasnetwork"
+}
 
 
-module.exports = { updateInfo, database }
+//console.log(database.adasnetwork.welcomeMessage)
+module.exports = { updateInfo, database, workspaceChecker }
 
--- src/callingResources.js
+++ src/callingResources.js
@@ -1,68 +1,102 @@
 require("dotenv").config();
-
-const callResources = async (app, ack, body) => {
+const {database, workspaceChecker} = require('../config/constants')
+const callResources = async (app, ack, body, workspaceChecker, database) => {
+    //console.log(body);
+    //console.log(database)
+    //console.log(workspaceChecker);
+    //console.log(body.team_domain);
+    workspaceID = ''
+    //FIND THE PROPER WORKSPACE CHANNEL
+    for (const [key, value] of Object.entries(workspaceChecker)) {
+        if(key == body.team_domain){
+            workspaceID = value
+        }
+      }
+    //console.log(typeof(workspaceID))
+    const workspaceObject = database[workspaceID];
+    //console.log(workspaceID)
+    //console.log(workspaceObject);
+    blockList = [
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "If this is your first time using Slack and you're confused by its interface, learn more about it here -->"
+            },
+            "accessory": {
+                "type": "button",
+                "text": {
+                    "type": "plain_text",
+                    "text": "Click Me!",
+                    "emoji": true
+                },
+                "value": "click_me_123",
+                "url": "https://slack.com/resources/slack-101",
+                "action_id": "resource-button-action"
+            }
+        },
+        {
+            "type": "header",
+            "text": {
+                "type": "plain_text",
+                "text": "Ada's Team Resources:",
+                "emoji": true
+            }
+        },
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": " <https://www.adasteam.ca/|Adas Team Website>"
+            }
+        },
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "<https://www.instagram.com/adas_team/|Adas Team Instagram : @adas_team>"
+            }
+        },
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "<https://www.linkedin.com/company/adas-team/|Adas Team LinkedIn>"
+            }
+        },
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "<https://www.facebook.com/AdasTeamFB/|Adas Team Facebook>"
+            }
+        }
+    ]
+    var resourcesList = []
+    //console.log(workspaceObject.resources)
+    for (const [key, value] of Object.entries(workspaceObject.resources)) {
+        resourcesList.push(key,value)
+        //console.log(`${key} : ${value}`)
+      }
+    for(i = 1; i <= resourcesList.length - 1 ; i+=2){
+        blockSection = {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": `<${resourcesList[i].url}|${resourcesList[i].text}>`
+            }
+        }
+        blockList.push(blockSection)
+        //console.log(resourcesList[i])
+    }
+    //console.log(resourcesList)
 	await ack();
 	await app.client.chat.postEphemeral({
 			token: process.env.SLACK_BOT_TOKEN,
 			channel: body.channel_id,
 			user: body.user_id,
             //text: "If this is your first time using Slack and you're confused by its interface, learn more about it here: https://slack.com/resources/slack-101 ",
-            "blocks": [
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": "If this is your first time using Slack and you're confused by its interface, learn more about it here -->"
-                    },
-                    "accessory": {
-                        "type": "button",
-                        "text": {
-                            "type": "plain_text",
-                            "text": "Click Me!",
-                            "emoji": true
-                        },
-                        "value": "click_me_123",
-                        "url": "https://slack.com/resources/slack-101",
-                        "action_id": "resource-button-action"
-                    }
-                },
-                {
-                    "type": "header",
-                    "text": {
-                        "type": "plain_text",
-                        "text": "Ada's Team Resources:",
-                        "emoji": true
-                    }
-                },
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": " <https://www.adasteam.ca/|Adas Team Website>"
-                    }
-                },
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": "<https://www.instagram.com/adas_team/|Adas Team Instagram : @adas_team>"
-                    }
-                },
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": "<https://www.linkedin.com/company/adas-team/|Adas Team LinkedIn>"
-                    }
-                },
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": "<https://www.facebook.com/AdasTeamFB/|Adas Team Facebook>"
-                    }
-                }
-            ]
+            "blocks": blockList
 	})
 };
 
